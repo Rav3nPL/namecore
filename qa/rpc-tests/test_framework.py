6b2745934695f57cbb049a03c26e119e281a807a
Daniel Kraft <d@domob.eu>
2015-04-09 21:43:14 +0200

    Add unit tests for UNO trie.
    Also fix a bug found with the testing.

--- qa/rpc-tests/test_framework.py
+++ qa/rpc-tests/test_framework.py
@@ -34,17 +34,17 @@ class BitcoinTestFramework(object):
     def getExtraArgs(self, n):
         """
         Provide extra args to pass to node n when starting it.  By default,
-        only set -namehistory for nodes 1 and 2, so that we can test
-        both cases (with history and without).  Subclasses can override
+        only set -namehistory and -unotrie for nodes 1 and 2, so that we can
+        test both cases (with the options and without).  Subclasses can override
         this method to build a custom-configured network.
         """
 
-        # We choose nodes 1 and 2 to keep -namehistory, because this allows
-        # us to test both nodes with it and without it in both split
+        # We choose nodes 1 and 2 to get the extra options, because this allows
+        # us to test both nodes with it and without them in both split
         # network parts (0/1 vs 2/3).
 
         if n == 1 or n == 2:
-            return ["-namehistory"]
+            return ["-namehistory", "-unotrie"]
 
         return []
 
